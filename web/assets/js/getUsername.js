33a787d9dc5e3e916eaa801b1c2cc20c159cf0e7
Henri-Thibault Huyghe <Henri-Thibault.Huyghe@student.howest.be>
2017-11-18 16:32:51 +0100

    work

---
+++ web/assets/js/getUsername.js
@@ -0,0 +1,16 @@
+function getAndShowUsername() {
+  $.ajax({url: "getUsername", success: function(result){
+    console.log(result);
+    if($("#user").length != 0) {
+      $("#user").html(result);
+    }
+
+    if($(".user-view p").length != 0) {
+      $(".user-view p").html(result);
+    }
+  }});
+}
+
+$(document).ready(function () {
+  getAndShowUsername();
+})
