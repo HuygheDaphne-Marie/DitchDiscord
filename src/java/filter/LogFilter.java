a615f25bb90708c778111a89347f34ed5c847fbd
BrechtWillemsTI <brecht.willems2@student.howest.be>
2017-11-18 17:21:17 +0100

    changed filter.

--- src/java/filter/LogFilter.java
+++ src/java/filter/LogFilter.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Brecht
  */
-@WebFilter(filterName = "LogFilter", urlPatterns = {"/register", "/deleteUser", "/index.html", "/register.html", "/login", "/getUsername", "/chatPage"})
+@WebFilter(filterName = "LogFilter", urlPatterns = {"/index.html", "/register.html", "/register", "/login", "/deleteUser", "/administratorLoginPage.html", "/administratorPage.html"})
 public class LogFilter implements Filter {
     
     private static final boolean debug = true;
