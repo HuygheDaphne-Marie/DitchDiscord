d0b37383465f36252ef730b02d89b81686fd606e
SchramMichiel <michiel.schram@student.howest.be>
2017-11-20 00:23:24 +0100

    Laatste fixes

--- src/java/servlets/LogoutServlet.java
+++ src/java/servlets/LogoutServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package be.howest.ti.filterslogindemo2.servlets;
+package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
