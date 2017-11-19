09c2098ddfca5b3db8e98008a5df024dfc4a40f1
VerriestFredrik <fredrik.verriest@student.howest.be>
2017-11-19 17:43:57 +0100

    recaptcha response added from google, now works (needs revising)

--- src/java/servlets/login.java
+++ src/java/servlets/login.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.berry.BCrypt;
+import utils.VerifyRecaptcha;
 
 /**
  *
@@ -21,7 +22,8 @@ import com.berry.BCrypt;
  */
 @WebServlet(name = "login", urlPatterns = {"/login"})
 public class login extends HttpServlet {
-     public static final String SESS_USER = "USER";
+
+    public static final String SESS_USER = "USER";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -36,30 +38,30 @@ public class login extends HttpServlet {
             throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+        System.out.println(gRecaptchaResponse);
+        boolean verify = VerifyRecaptcha.verify(gRecaptchaResponse);
         User u = Repositories.getUserRepository().getUserByUsername(username);
-        
-        if(u !=null)
-        {
-            if (BCrypt.checkpw(password, u.getPassword()))
-            {
+
+        if (u != null) {
+            if (BCrypt.checkpw(password, u.getPassword()) && verify) {
                 System.out.println("It matches");
                 request.getSession().setAttribute("username", u.getName());
                 request.getSession().setAttribute("password", u.getPassword());
-                 request.getSession().setAttribute(SESS_USER, u.getName());
-                 response.sendRedirect("chatPage.html");
+                request.getSession().setAttribute(SESS_USER, u.getName());
+                response.sendRedirect("chatPage.html");
+            } else {
+                response.sendRedirect("index.html");
             }
-             else
-                {
-             response.sendRedirect("index.html");
-                 }
-        }
-        else
-        {
+        } else {
             response.sendRedirect("index.html");
         }
-        
-        
-        
+        if (verify) {
+            System.out.println("Either user name or password is wrong.");
+        } else {
+            System.out.println("You missed the Captcha.");
+        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
